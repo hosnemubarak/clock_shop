e57c324175f5972d82e37ff22ae6fe95c018757a
Hosne Mubarak <h.m.tasmir.cse@gmail.com>
2026-01-12 18:14:38 +0600

    fix image issue

--- clock_shop/clock_shop/urls.py
+++ clock_shop/clock_shop/urls.py
@@ -2,9 +2,10 @@
 URL configuration for clock_shop project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,6 +17,11 @@ urlpatterns = [
     path('reports/', include('apps.reports.urls')),
 ]
 
+# Serve media files in both development and production
+# In production with high traffic, consider using a reverse proxy (nginx) instead
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
